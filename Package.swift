5db21cebbe12ec0c9b9d0364147a1a8da78c42cc
Andrey Sak <andrei.sak@banuba.com>
2025-01-17 12:37:41 +0300

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@
 
 import PackageDescription
 
+let faceARversionRange: Range<Version> = "1.16.0"..<"1.16.3"
+let videoEditorSDKRange: Range<Version> = "1.39.0"..<"1.49.99"
+
 let package = Package(
   name: "BanubaSdk",
   platforms: [
@@ -11,6 +14,20 @@ let package = Package(
     .library(
       name: "BanubaSdk",
       targets: ["BanubaSdk"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/sdk-banuba/BNBSdkCore.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBSdkApi.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBEffectPlayer.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBScripting.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBLips.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBHair.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBEyes.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBBackground.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBSkin.git", faceARversionRange),
+    
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", videoEditorSDKRange),
+    .package(url: "https://github.com/Banuba/BanubaVideoEditorCore-iOS.git", videoEditorSDKRange)
   ],
   targets: [
     .binaryTarget(
