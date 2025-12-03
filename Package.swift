1e0c5293e0dd5372e36c142f5e2976aa7c38464a
CI <ci@banuba.com>
2025-12-03 18:07:35 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let faceARversionRange: Range<Version> = "1.17.2"..<"1.17.99"
-let version: Version = "1.49.0"
+let version: Version = "1.49.1-beta.0"
 
 let package = Package(
   name: "BanubaSdk",
