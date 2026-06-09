8f663076df902a8865eca20f23ad3860aca3a222
CI <ci@banuba.com>
2026-06-09 12:17:20 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,8 +2,9 @@
 
 import PackageDescription
 
-let faceARversionRange: Range<Version> = "1.18.0"..<"1.18.99"
-let version: Version = "1.52.1"
+let faceARversionRange: Range<Version> = "1.17.0"..<"1.17.99"
+let banubaUtilsVersionRange: Range<Version> = "1.53.0"..<"1.53.99"
+let banubaCoreVersionRange: Range<Version> = "1.53.0"..<"1.53.99"
 
 let package = Package(
   name: "BanubaSdk",
@@ -17,9 +18,9 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
-    .package(url: "https://github.com/Banuba/BanubaVideoEditorCore-iOS.git", exact: version),
-    
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", banubaUtilsVersionRange),
+    .package(url: "https://github.com/Banuba/BanubaVideoEditorCore-iOS.git", banubaCoreVersionRange),
+
     .package(url: "https://github.com/sdk-banuba/BNBSdkCore.git", faceARversionRange),
     .package(url: "https://github.com/sdk-banuba/BNBSdkApi.git", faceARversionRange),
     .package(url: "https://github.com/sdk-banuba/BNBEffectPlayer.git", faceARversionRange),
@@ -31,9 +32,17 @@ let package = Package(
     .package(url: "https://github.com/sdk-banuba/BNBSkin.git", faceARversionRange)
   ],
   targets: [
+    // Local development uses the path-based binaryTarget below.
+    // Release flow (fastlane patch_package_swift_for_nexus) replaces it in the GitHub distribution repo with:
+    // .binaryTarget(
+    //   name: "BanubaSdk",
+    //   url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaSDK/{version}/BanubaSdk-{version}.xcframework.zip",
+    //   checksum: "{computed at release}"
+    // )
     .binaryTarget(
       name: "BanubaSdk",
-      path: "BanubaSdk.xcframework"
+      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaSDK/1.53.0-beta.1/BanubaSdk-1.53.0-beta.1.xcframework.zip",
+      checksum: "dc54ec4335183958f9bf9a28f7d5768efdb23c724ee37a0657a2ffce87b3e2cb"
     ),
     .target(
       name: "BanubaSdkTarget",
